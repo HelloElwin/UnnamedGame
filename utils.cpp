b8b2e87b87eedaa6668b0392ab5aeffac779c41f
Kelios <91117956+Kelios1556@users.noreply.github.com>
2022-03-06 21:45:03 +0800

    Add files via upload

--- utils.cpp
+++ utils.cpp
@@ -7,18 +7,18 @@
 // 0    air
 // 1    ground
 
-// 2	player/bar/portal
-//		property = 'i' : ice 32
-//		property = 'f' : fire 1
+// 2  player/bar/portal
+//    property = 'i' : ice 32
+//    property = 'f' : fire 1
 
-//		property = 'r' : red 9
-//		property = 'p' : pink 219
-//		property = 'v' : violet 93
-//		property = 'b' : blue 12
-//		property = 'c' : cyan 14
-//		property = 'g' : green 118
-//		property = 'y' : yellow 226
-//		property = 'o' : orange 214
+//    property = 'r' : red 9
+//    property = 'p' : pink 219
+//    property = 'v' : violet 93
+//    property = 'b' : blue 12
+//    property = 'c' : cyan 14
+//    property = 'g' : green 118
+//    property = 'y' : yellow 226
+//    property = 'o' : orange 214
 
 
 void super_print(int type, char propty) {
@@ -29,50 +29,50 @@ void super_print(int type, char propty) {
     case 1:
       printf("\033[48;5;231m \033[0m");  
       break;
-		case 2:
-			switch (propty) {
-				case 'i':
-					printf("\033[48;5;32m \033[0m");  
-					break;
-				case 'f':
-					printf("\033[48;5;1m \033[0m");  
-					break;
-				case 'r':
-					printf("\033[48;5;9m \033[0m");  
-					break;
-				case 'p':
-					printf("\033[48;5;219m \033[0m");  
-					break;
-				case 'v':
-					printf("\033[48;5;93m \033[0m");  
-					break;
-				case 'b':
-					printf("\033[48;5;12m \033[0m");  
-					break;
-				case 'c':
-					printf("\033[48;5;14m \033[0m");  
-					break;
-				case 'g':
-					printf("\033[48;5;118m \033[0m");  
-					break;
-				case 'y':
-					printf("\033[48;5;226m \033[0m");  
-					break;
-				case 'o':
-					printf("\033[48;5;214m \033[0m");  
-					break;
-			}
-	}
+    case 2:
+      switch (propty) {
+        case 'i':
+          printf("\033[48;5;32m \033[0m");  
+          break;
+        case 'f':
+          printf("\033[48;5;1m \033[0m");  
+          break;
+        case 'r':
+          printf("\033[48;5;9m \033[0m");  
+          break;
+        case 'p':
+          printf("\033[48;5;219m \033[0m");  
+          break;
+        case 'v':
+          printf("\033[48;5;93m \033[0m");  
+          break;
+        case 'b':
+          printf("\033[48;5;12m \033[0m");  
+          break;
+        case 'c':
+          printf("\033[48;5;14m \033[0m");  
+          break;
+        case 'g':
+          printf("\033[48;5;118m \033[0m");  
+          break;
+        case 'y':
+          printf("\033[48;5;226m \033[0m");  
+          break;
+        case 'o':
+          printf("\033[48;5;214m \033[0m");  
+          break;
+      }
+  }
   // printf("\033[0;30;43m Hello \033[0m");
 }
 /*int main() {
-	int a;
-	char b;
-	while(true) {
-		std::cin>>a>>b;
-		super_print(a, b);
-		printf("\n");
-	}
-	return 0;
+  int a;
+  char b;
+  while(true) {
+    std::cin>>a>>b;
+    super_print(a, b);
+    printf("\n");
+  }
+  return 0;
 }
 /*
